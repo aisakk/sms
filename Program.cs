1f2ed944cd9b62368ece9042be3081aec25f7f37
aisakk <santaellaisaac2@gmail.com>
2024-09-01 10:42:58 -0400

    Enviar sms, falta incorporar el token de azure de communication services

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Communication;
+using Azure.Communication.Sms;
+
+//Falta incorporar el token o string de Azure
+string connectionString = <your-connection-string>;
+SmsClient smsClient = new SmsClient(connectionString);
+
+smsClient.Send(
+   from: new PhoneNumber("<leased-phone-number>"),
+   to: new PhoneNumber("<to-phone-number>"),
+   message: "Hello World via SMS"
+);
+
+//Ejecutar dotnet run 
+Console.WriteLine("Hello, World!");
